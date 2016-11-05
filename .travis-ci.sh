6cbd22b41debddec54652a9a181cdac8104a74ce
Karl Palmskog <palmskog@gmail.com>
2016-11-04 19:49:17 -0500

    Travis integration

---
+++ .travis-ci.sh
@@ -0,0 +1,26 @@
+set -e
+
+pushd ..
+wget 'http://homes.cs.washington.edu/~jrw12/coq-8.5-build-local.tgz'
+tar xf coq-8.5-build-local.tgz
+export PATH=$PWD/coq-8.5/bin:$PATH
+popd
+
+./build.sh
+
+case $DOWNSTREAM in
+verdi)
+  pushd ..
+    git clone 'http://github.com/uwplse/StructTact'
+    pushd StructTact
+      ./build.sh
+    popd
+
+    git clone 'http://github.com/uwplse/verdi'
+    pushd verdi
+      ./build.sh
+    popd
+  popd
+  ;;
+esac
+
