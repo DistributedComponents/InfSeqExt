7f4412c4a84333b50e6d22032b73aa12f880b908
Karl Palmskog <palmskog@gmail.com>
2016-11-16 21:01:49 -0600

    use specific Coq and ssreflect versions for Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,13 +1,13 @@
 opam init --yes --no-setup
 eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
-opam install coq --yes
+opam install coq.$COQ_VERSION --yes --verbose
 
 ./build.sh
 
 case $DOWNSTREAM in
 verdi)
-  opam install coq-mathcomp-ssreflect --yes
+  opam install coq-mathcomp-ssreflect.$SSREFLECT_VERSION --yes --verbose
 
   pushd ..
     git clone 'http://github.com/uwplse/StructTact'
