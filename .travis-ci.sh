6b47978306cc9660330278dce3cc06f34fcb32fd
Karl Palmskog <palmskog@gmail.com>
2017-12-20 16:42:14 -0600

    use Coq 8.7.1 in Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,11 +6,11 @@ eval $(opam config env)
 opam repo add coq-released https://coq.inria.fr/opam/released
 opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
 
-opam pin add coq $COQ_VERSION --yes --verbose
+opam pin add coq ${COQ_VERSION} --yes
 
 opam pin add InfSeqExt . --yes --verbose
 
-case $DOWNSTREAM in
+case ${DOWNSTREAM} in
 verdi)
   opam install verdi --yes --verbose
   ;;
